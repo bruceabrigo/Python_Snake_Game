b0f27d792c0f53587153258291528a8f44917d7d
Bruce Abrigo <bruceabrigo@outlook.com>
2024-01-22 16:54:23 -0600

    Food is randomly generating on canvas

--- main.py
+++ main.py
@@ -11,13 +11,26 @@ SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF00FF"
 BACKGROUND_COLOR = "#000000"
 
-# define a snake object
+# define a snake class
 class Snake:
     pass
 
-# define a food object
+# define a food class
 class Food:
-    pass
+    
+    def __init__(self):
+        # ensure food spawns at random x and y coordinates
+        # x and y math explained (start range at 0, WIDTH/HEIGHT divided by GAMESPACE = 14. Leaving 14 possible random spots on the board)
+        # multiplying by space size will convert the randomizer to select random pixels
+        x = random.randint(0, int(GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, int(GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE
+
+        # food coordinates should take a list of x and y
+        self.coordinates = [x, y]
+
+        # generate food to the canvas
+        # (starting corner, starting corner, ending corner, ending corner, fill color, tag)
+        canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 # define a turn function
 def next_turn():
@@ -64,10 +77,18 @@ screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
 # calculate the x axis
+# window geometry cannot use floats
+# we must specifiy x and y variables as strict integers
 x = int((screen_width/2) - (window_width/2))
 # calculate the y axis
 y = int((screen_height/2) - (window_height/2))
 
+# utilize window geometry to center window
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# define our snake and food objects
+snake = Snake()
+food = Food()
+
 # start the game loop
 window.mainloop()
